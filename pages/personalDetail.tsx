5f6d4d0df438dfafc1186cb62e43474c8cedd575
Christopher Alphonse Cervantes <cervantes.alphonse@yahoo.com>
2024-06-05 10:54:26 +1000

    show user detail is working

--- pages/personalDetail.tsx
+++ pages/personalDetail.tsx
@@ -1,61 +1,78 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 import { HttpMethod } from "../types";
 import { ItemData } from "./showAllUsers";
 
 export function PersonalDetail({ route }: any) {
   const params = route.params;
-
+  const [user, setUser] = useState<ItemData>();
   let data: ItemData | undefined;
 
   useEffect(() => {
-    const fetchUsers = async () =>
-      await fetch(`http://localhost:5235/api/consumer/${params.id}`, {
-        method: HttpMethod.Get,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((responseData: ItemData) => {
-          const user = JSON.parse(JSON.stringify(responseData));
+    const fetchUser = async () => {
+      let response;
+      let userResponse: ItemData | undefined = undefined;
+      try {
+        response = await fetch(
+          `http://localhost:5235/api/consumer/${params.id}`,
+          {
+            method: HttpMethod.Get,
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        userResponse = (await response.json()) as ItemData;
+      } catch (error) {
+        alert(`error fetching the data ${error}`);
+      }
 
-          data = {
-            id: user.id,
-            accountName: user.accountName,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            birthDate: user.birthDate,
-            mobile: user.mobile,
-          };
-        })
-        .catch((error) => {
-          alert(`error fetching ${error}`);
-        });
+      setUser(userResponse);
+    };
+    fetchUser();
+  }, []);
 
-    fetchUsers();
-  });
-
-  alert(`test ${data}`);
+  let birthdate = "";
+  if (user && user.birthDate) {
+    birthdate = `${new Date(user.birthDate).getDay()}/${new Date(
+      user.birthDate
+    ).getMonth()}/${new Date(user.birthDate).getFullYear()}`;
+  }
 
   return (
-    <View style={styles.display}>
-      <Text>{data?.accountName}</Text>
-      <Text>{data?.firstName}</Text>
-      <Text>{params.lastName}</Text>
-      <Text>{params.birthDate}</Text>
+    <View style={{ backgroundColor: "#fff0", paddingTop: 10 }}>
+      <TextInput
+        style={{ ...styles.text }}
+        value={user?.accountName}
+      ></TextInput>
+      <Text style={styles.text}>{user?.accountName}</Text>
+      <Text style={{ ...styles.text, fontSize: 16 }}>{user?.firstName}</Text>
+      <Text style={{ ...styles.text }}>{user?.lastName}</Text>
+      <Text style={{ ...styles.text }}>{birthdate}</Text>
+      <View style={{ ...styles.button }}>
+        <Button title="Update"></Button>
+
+        <Button title="Delete"></Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   display: {
-    height: 40,
     marginBottom: 10,
     backgroundColor: "#fff",
     padding: 10,
+  },
+  text: {
+    fontSize: 14,
+    opacity: 0.8,
+    marginLeft: 15,
+    color: "#0099cc",
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
